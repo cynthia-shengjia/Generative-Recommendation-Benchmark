78c8ba86a29c57dcf4dae05fc1dff0804de2d150
cynthia-shengjia <1455319292@qq.com>
2025-09-02 10:41:08 +0800

    fix: delete evaluate in traier

--- genrec/trainers/tokenizer_trainers/TokenizerTrainers.py
+++ genrec/trainers/tokenizer_trainers/TokenizerTrainers.py
@@ -4,36 +4,14 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-
+from genrec.optimizers.tokenizer_optimizers.TokenzierOptimizer import AbstractTokenizerOptimizer
+from genrec.tokenizers.GRTokenizer import AbstractTokenizer
 class Trainer:
-    """
-    A class that handles the training process for a model.
-
-    Args:
-        config (dict): The configuration parameters for training.
-        model (AbstractModel): The model to be trained.
-        tokenizer (AbstractTokenizer): The tokenizer used for tokenizing the data.
-
-    Attributes:
-        config (dict): The configuration parameters for training.
-        model (AbstractModel): The model to be trained.
-        evaluator (Evaluator): The evaluator used for evaluating the model.
-        logger (Logger): The logger used for logging training progress.
-        project_dir (str): The directory path for saving tensorboard logs.
-        accelerator (Accelerator): The accelerator used for distributed training
-        saved_model_ckpt (str): The file path for saving the trained model checkpoint.
-
-    Methods:
-        fit(train_dataloader, val_dataloader): Trains the model using the provided training and validation dataloaders.
-        evaluate(dataloader, split='test'): Evaluate the model on the given dataloader.
-        end(): Ends the training process and releases any used resources.
-    """
-
     def __init__(
         self, 
         config: dict,  
         tokenizer: AbstractTokenizer,
-        optimizer: 
+        optimizer: AbstractTokenizerOptimizer
     ):
         self.config     = config
         self.tokenizer  = tokenizer 
@@ -48,20 +26,6 @@ class Trainer:
             val_dataloader: The dataloader for validation data.
         """
         raise NotImplementedError('Trainer fit not implemented.')
-
-    def evaluate(self, dataloader, split='test'):
-        """
-        Evaluate the model on the given dataloader.
-
-        Args:
-            dataloader (torch.utils.data.DataLoader): The dataloader to evaluate on.
-            split (str, optional): The split name. Defaults to 'test'.
-
-        Returns:
-            OrderedDict: A dictionary containing the evaluation results.
-        """
-        raise NotImplementedError("Trainer evaluate not implemented")
-
 
 
 
